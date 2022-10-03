93075cc8475fddbe7e80fc5d8995502d052569a6
Jorge Ramirez <jorgeram@herbalife.com>
2022-10-03 17:12:38 -0500

    Formulario simple

--- App.tsx
+++ App.tsx
@@ -1,119 +1,72 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {type PropsWithChildren} from 'react';
+import React from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
+  TextInput,
   View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import Button from './button';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={'dark-content'} backgroundColor={'black'} />
+      <View style={styles.formContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Name"
+          placeholderTextColor={'gray'}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Email"
+          placeholderTextColor={'gray'}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Phone"
+          placeholderTextColor={'gray'}
+        />
+      </View>
+      <View style={styles.buttonsContainer}>
+        <Button style={styles.button} title="Validar" />
+        <Button style={styles.button} title="Limpiar" />
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    backgroundColor: 'gray',
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  formContainer: {
+    marginTop: 40,
+    backgroundColor: 'lightgrey',
+    borderWidth: 1,
+    borderColor: 'white',
+    marginHorizontal: 20,
+    marginBottom: 40,
+    padding: 10,
+    paddingHorizontal: 40,
+    paddingBottom: 50,
+    borderRadius: 5,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  textInput: {
+    marginTop: 20,
+    padding: 5,
+    borderBottomColor: 'black',
+    borderBottomWidth: 2,
   },
-  highlight: {
-    fontWeight: '700',
+  buttonsContainer: {
+    marginHorizontal: 20,
+    flexDirection: 'row',
+  },
+  button: {
+    flex: 1,
+    marginHorizontal: 10,
   },
 });
 
---
+++ button.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  StyleProp,
+  ViewStyle,
+  TextStyle,
+} from 'react-native';
+
+export interface ButtonProps {
+  title: string;
+  onPress?: () => void;
+  style?: StyleProp<ViewStyle>;
+  titleStyle?: StyleProp<TextStyle>;
+}
+
+const Button = (props: ButtonProps) => {
+  return (
+    <TouchableOpacity
+      style={[styles.container, props.style]}
+      onPress={props.onPress}>
+      <Text style={[styles.title, props.titleStyle]}>{props.title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'lightgreen',
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  title: {
+    color: 'black',
+    fontWeight: '600',
+    fontSize: 16,
+  },
+});
+
+export default Button;
